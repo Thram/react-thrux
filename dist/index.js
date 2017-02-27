2c6f387a5c7456177f3b0bf8ec672efdb0d92b64
Thram <thramposo@gmail.com>
2017-02-27 20:59:12 +1300

    fix(builds): update builds

--- dist/index.js
+++ dist/index.js
@@ -17,14 +17,13 @@ var _assign = require("lodash/assign");
 
 var _assign2 = _interopRequireDefault(_assign);
 
-var _isArray = require("lodash/isArray");
-
-var _isArray2 = _interopRequireDefault(_isArray);
-
 var _thrux = require("thrux");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * Created by thram on 21/01/17.
+ */
 var connect = exports.connect = function connect(stateKey, ReactComponent) {
   return _react2.default.createClass({
     observers: {},
@@ -34,15 +33,14 @@ var connect = exports.connect = function connect(stateKey, ReactComponent) {
     componentDidMount: function componentDidMount() {
       var _this = this;
 
-      var addObserver = function addObserver(key) {
+      (0, _forEach2.default)([].concat(stateKey), function (key) {
         _this.observers[key] = function (state) {
           var newState = {};
           newState[key] = state;
           _this.setState(newState);
         };
         (0, _thrux.observe)(key, _this.observers[key]);
-      };
-      (0, _isArray2.default)(stateKey) ? (0, _forEach2.default)(stateKey, addObserver) : addObserver(stateKey);
+      });
     },
     componentWillUnmount: function componentWillUnmount() {
       (0, _forEach2.default)(this.observers, function (observer, key) {
@@ -54,6 +52,4 @@ var connect = exports.connect = function connect(stateKey, ReactComponent) {
       return _react2.default.createElement(ReactComponent, (0, _assign2.default)({}, this.props, this.state));
     }
   });
-}; /**
-    * Created by thram on 21/01/17.
-    */
+};
