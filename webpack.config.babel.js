bb8e4b4cfa3adbe2b33bf0d833b984931cdc7d49
Thram <thramposo@gmail.com>
2017-03-27 21:45:00 +1300

    fix(dependencies): Update dependencies

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,21 +5,27 @@ import { join } from 'path';
 
 const include = join(__dirname, 'src');
 
-export default {
-  entry: './src/index.jsx',
-  output: {
-    path: join(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    library: 'ReactThrux',
-  },
-  devtool: 'source-map',
-  externals: {
-    thrux: 'thrux',
-    react: 'React'
-  },
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, loader: 'babel-loader', include },
-    ],
-  },
-};
+export default function (env = {}) {
+  return {
+    entry: { 'react-thrux': './src/index' },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    output: {
+      path: join(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      library: 'ReactThrux',
+      filename: env.prod ? '[name].umd.min.js' : '[name].umd.js',
+    },
+    devtool: 'source-map',
+    externals: {
+      thrux: 'thrux',
+      react: 'React',
+    },
+    module: {
+      loaders: [
+        { test: /\.jsx?$/, loader: 'babel-loader', include },
+      ],
+    },
+  };
+}
